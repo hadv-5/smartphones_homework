851daebeb89d3e9bfd943ae486ad88bef578a786
abdalhadi saad <abdalhadiml6@gmail.com>
2026-05-19 22:23:45 +0300

    Add files via upload

---
+++ homework-5/lib/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static String url = "http://localhost/flutter_login/check.php";
+  static String regis = "http://localhost/flutter_login/register.php";
+  static String products = "http://localhost/flutter_login/get_products.php";
+}
